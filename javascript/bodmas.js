cf1ade7ca337d3de0fb9e2f4ea50945219b974b3
SHAIK MOHAMMAD RAFI <shaik.mohammadrafi2712@gmail.com>
2026-07-28 21:33:20 +0530

    added bodmas

---
+++ javascript/bodmas.js
@@ -0,0 +1,12 @@
+// priority ascending order
+ // ------ BODMAS ------------
+// b -Brackets
+// o -order -->exponents , powers Etc.
+// d - divison
+// m - multiplication
+// a - addition
+// s - subtraction
+
+let a = (10+2)*10 ;
+ console.log(a);
+ 
--- javascript/typecasting.js
+++ javascript/typecasting.js
@@ -1,5 +1,4 @@
 //You manually convert a value using JavaScript functions.//
-
 let a = 1234 ;
 console.log(a);
 console.log(typeof a);
@@ -12,3 +11,13 @@ console.log(typeof b);
 console.log(true + 5);
 // ------------ false = 0 ----------
 console.log(false + 5);
+// parseint
+// it takes only number no spaces ,no characters ect .. 
+// once the numbers stops it stops recursions 
+
+let c = "123abc" ;
+console.log(c);
+console.log(typeof c);
+
+ let d=parseInt(c);
+ console.log(d);

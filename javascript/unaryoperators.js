43002ab00d08d9805802823143cfe32973ed96fc
SHAIK MOHAMMAD RAFI <shaik.mohammadrafi2712@gmail.com>
2026-07-28 22:27:48 +0530

    added pre and post increments

---
+++ javascript/unaryoperators.js
@@ -0,0 +1,12 @@
+// pre increment change,then use...
+let a = 10 ;
+let newage =++a;
+console.log(a);
+console.log(newage);
+
+//post increment use and then change...
+let b = 10 ;
+let newag =b++;
+console.log(newag);
+
+console.log(b);

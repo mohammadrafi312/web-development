f1a08c1558cd3fbbc7da4b9d02e51b1f294f29fc
SHAIK MOHAMMAD RAFI <shaik.mohammadrafi2712@gmail.com>
2026-07-28 20:21:47 +0530

    added files

--- javascript/datatypes.js
+++ javascript/datatypes.js
@@ -3,3 +3,23 @@ let a =20;
 console.log(a);
 //type checking of integers 
 console.log(typeof a);
+//infinity positive and negative 
+let b = 1/0;
+let c = -1/0;
+//nan string divided by integer we get NaN
+let d = "string"/2 ;
+console.log(a);
+console.log(typeof d);
+// string data type 
+let name = "Rafi";
+console.log(name);
+console.log(typeof name);
+// boolean 
+let boolean = true ;
+console.log(boolean);
+console.log(typeof boolean);
+// big int
+let bigNumber = 123456789012345678901234567890n;
+
+console.log(bigNumber);
+console.log(typeof bigNumber);
---
+++ javascript/typecasting.js
@@ -0,0 +1,14 @@
+//You manually convert a value using JavaScript functions.//
+
+let a = 1234 ;
+console.log(a);
+console.log(typeof a);
+//converting the number to string..
+let b =String(a);
+console.log(b);
+console.log(typeof b);
+
+// ---------  true = 1  ---------
+console.log(true + 5);
+// ------------ false = 0 ----------
+console.log(false + 5);

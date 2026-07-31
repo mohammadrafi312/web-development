60960538371d9b4ec0aec964f03d37d5a724dc62
SHAIK MOHAMMAD RAFI <shaik.mohammadrafi2712@gmail.com>
2026-07-31 11:05:05 +0530

    added template literal

---
+++ javascript/templateliterals.js
@@ -0,0 +1,6 @@
+
+let pencilprice = 10 ;
+let eraserprice = 5 ;
+// let total = pencilprice + eraserprice ;
+let total =`the total price : ${pencilprice + eraserprice} rupees only.` ;
+console.log(total);

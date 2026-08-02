8e37643a1b3a9bb1d9cec5e67d2f1dc91893b5c7
SHAIK MOHAMMAD RAFI <shaik.mohammadrafi2712@gmail.com>
2026-08-02 21:10:04 +0530

    edited array methods files

--- javascript/arraymethods.js
+++ javascript/arraymethods.js
@@ -1,7 +1,21 @@
 let fruits = ["apple", "banana", "cherry", "date", "elderberry"];
-// push and pop methods
-fruits.push("blackberry");// add from end 
-console.log(fruits);
+// // push and pop methods
+// fruits.push("blackberry");// add from end 
+// console.log(fruits);
 
-fruits.pop(); // remove from end
-console.log(fruits);
+// fruits.pop(); // remove from end
+// console.log(fruits);
+
+// // unshift and shift methods
+// fruits.unshift("fig");
+// // console.log(fruits);
+// console.log(fruits.shift());
+// console.log(fruits.shift());
+//  // remove from start
+// console.log(fruits);
+
+//reverse and sort methods
+console.log(fruits.reverse());
+let num = [5, 2, 9, 1, 5, 6];
+console.log(num.sort());
+

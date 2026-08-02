858b5d5a056e6453219d678e4da47e2b52ded40a
SHAIK MOHAMMAD RAFI <shaik.mohammadrafi2712@gmail.com>
2026-08-02 20:07:56 +0530

    added files

---
+++ javascript/stringmethods.js
@@ -0,0 +1,17 @@
+
+// trim 
+let str = " hello ";
+console.log(str.trim()); // "hello"
+
+// toUpperCase
+let str2 = "hello";
+console.log(str2.toUpperCase()); // "HELLO"
+
+//toLowerCase
+let str3 = "HELLO";
+console.log(str3.toLowerCase()); // "hello"
+
+//indexof
+
+let str4 = "hello world";
+console.log(str4.indexOf('e'));

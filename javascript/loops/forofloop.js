6aa6bb8cdec862d79e8c7dabd11520297449228a
SHAIK MOHAMMAD RAFI <shaik.mohammadrafi2712@gmail.com>
2026-08-04 18:20:45 +0530

    added and edit files

---
+++ javascript/loops/forofloop.js
@@ -0,0 +1,4 @@
+let fruits = ["apple", "banana", "mango", "grapes"];
+for(let i of fruits){
+    console.log(i);
+}
--- javascript/loops/nestedarrayloop.js
+++ javascript/loops/nestedarrayloop.js
@@ -1,2 +1,5 @@
 let arr =[["rafi","reshma"],["mabjani","meeravali"]];
-console.log(arr);
+for (let i=0; i<arr.length; i++){
+    // for(let j=0;j<arr[i].length;j++)
+        console.log(arr[i]);
+}

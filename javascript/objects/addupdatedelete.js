563d70e408198692ef0d3042300218c65977deb1
SHAIK MOHAMMAD RAFI <shaik.mohammadrafi2712@gmail.com>
2026-08-05 22:56:34 +0530

    added files

---
+++ javascript/objects/addupdatedelete.js
@@ -0,0 +1,14 @@
+let student = {
+    name: "John Doe",
+    age: 20,
+    class : 10,
+    course:"cbse"
+}
+console.log(student);
+student.name = "rafi";//updated 
+console.log(student);
+student.gender ="male";//added
+console.log(student);
+
+delete student.course;//deleted
+console.log(student);

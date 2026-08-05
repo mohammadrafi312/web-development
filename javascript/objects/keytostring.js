b3cc9365206eb63cf8bf11f8fe0e05b939f4bf61
SHAIK MOHAMMAD RAFI <shaik.mohammadrafi2712@gmail.com>
2026-08-05 22:48:07 +0530

    added files

---
+++ javascript/objects/keytostring.js
@@ -0,0 +1,9 @@
+// keys are automatically converted to strings in JavaScript objects
+let students ={
+    1: "John Doe",
+    2: "Jane Smith",
+    3: "Alice Johnson"
+}
+console.log(students[1]); // Output: John Doe
+console.log(students["2"]); // Output: Jane Smith
+console.log(students[3]); // Output: Alice Johnson 

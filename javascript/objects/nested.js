e8cadc67d95a9339e6c4932e50a76c7d95996fa2
SHAIK MOHAMMAD RAFI <shaik.mohammadrafi2712@gmail.com>
2026-08-05 23:12:04 +0530

    modifed nested js

--- javascript/objects/nested.js
+++ javascript/objects/nested.js
@@ -7,3 +7,9 @@ let profile ={
     }
 }
 console.log(profile);
+console.log(profile.address);//view nested 
+
+console.log(profile.address.doorno);//view by property ..
+// update inside nested loop
+profile.address.phno =9381939958 ;
+console.log(profile.address);

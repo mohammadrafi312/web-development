26f8736ecfb1ba976fb22b802847924520190426
SHAIK MOHAMMAD RAFI <shaik.mohammadrafi2712@gmail.com>
2026-08-05 23:22:55 +0530

    added array of objects

---
+++ javascript/objects/arrayofobjects.js
@@ -0,0 +1,13 @@
+let profile = [
+    {
+    name :"rafi",
+    email :"shiakrafi@gmail.com",
+    },
+    {
+        doorno :"0-0",
+        phno   :"9381939958"
+}
+]
+for (let i = 0;i<profile.length;i++){
+console.log(profile[i]);
+}

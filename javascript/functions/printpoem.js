c2efbfa50cdd45b756c8e8f2055874b68048dd07
SHAIK MOHAMMAD RAFI <shaik.mohammadrafi2712@gmail.com>
2026-08-06 17:59:00 +0530

    added

--- javascript/functions/printpoem.js
+++ javascript/functions/printpoem.js
@@ -1,3 +1,4 @@
 function poem(){
-    console.log("Twinkle, twinkle, little star,How I wonder what you are!Up above the world so high,Like a diamond in the sky.")
+    console.log("Twinkle, twinkle, little star,How I wonder what you are! Up above the world so highLike a diamond in the sky.")
 }
+poem();

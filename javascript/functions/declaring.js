a8145144ff12433dfb6dcbe297614c5b841b287a
SHAIK MOHAMMAD RAFI <shaik.mohammadrafi2712@gmail.com>
2026-08-06 17:53:19 +0530

    added declaring js file

---
+++ javascript/functions/declaring.js
@@ -0,0 +1,14 @@
+function functioname (){
+    console.log("Hello World");
+}
+// calling function
+functioname();
+
+//print 1 to 5 using function
+
+function print1to5 (){
+    for (let i=1;i<=5;i++){
+        console.log(i);
+    }
+}
+print1to5();

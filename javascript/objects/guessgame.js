9728675b620afbf06da16e7d77ea96462795f157
SHAIK MOHAMMAD RAFI <shaik.mohammadrafi2712@gmail.com>
2026-08-06 17:29:38 +0530

    modified guess game

--- javascript/objects/guessgame.js
+++ javascript/objects/guessgame.js
@@ -1,3 +1,17 @@
-let max =prompt("enter the maximum range");
-// let range = Math.floor(Math.random()* max ) + 1;
+let max = Number(prompt("Enter the maximum range:"));
+let random = Math.floor(Math.random()* max ) + 1;
+console.log(random);
+
 let guess =prompt("enter the random number ");
+console.log(guess);
+while(true){
+    if (guess === "quit") {
+    console.log("You quit the game.");
+    break;
+    }else if(guess == random){
+        guess =prompt("🎉 Congratulations! You guessed correctly.");
+        // break;
+    }else{
+        guess =prompt("wrong answer try again!");
+    }
+}

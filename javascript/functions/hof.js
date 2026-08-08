0994a053d1c22059135dc11e74f10bd0b600d2e1
SHAIK MOHAMMAD RAFI <shaik.mohammadrafi2712@gmail.com>
2026-08-08 22:22:18 +0530

    added

--- javascript/functions/hof.js
+++ javascript/functions/hof.js
@@ -1,20 +1,29 @@
-//hof -higher order functions are 
+// //hof -higher order functions are 
 
-//1)function calls another function 
+// //1)function calls another function 
 
-function greet(){
-    console.log("hello");
-}
-function execute(fn){
-    fn();
-}
-execute(greet);
+// function greet(){
+//     console.log("hello");
+// }
+// function execute(fn){
+//     fn();
+// }
+// execute(greet);
 
-//2)in return we cfall another function 
-function sum(){
-    return function add(){
-        console.log("added");
-          }
+// //2)in return we cfall another function 
+// function sum(){
+//     return function add(){
+//         console.log("added");
+//           }
+// }
+// let s =sum();
+// console.log(s);
+
+//check even or odd number
+
+let odd = function(n){
+ console.log(!(n%2==0));
 }
-let s =sum();
-console.log(s);
+let even = function(n){
+    console.log(n%2==0);
+}
